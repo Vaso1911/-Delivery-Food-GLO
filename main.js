fa43d172399359d4e537e11134e4a3166ab176b5
Alexandr <79134038382@mail.ru>
2025-09-14 11:31:46 +0300

    исправлены классы для карточек, сверстана страница ресторана, сделан переход на страницу  ресторана по клику на карточку , вывод названия карточки в заголовке

---
+++ main.js
@@ -0,0 +1,48 @@
+function init() {
+  function initCards() {
+    const cards = document.querySelectorAll('.card-link');
+
+    if (!cards.length) return;
+
+    cards.forEach(card => {
+      card.addEventListener('click', handleCardClick);
+    });
+  }
+
+  function handleCardClick(e) {
+    e.preventDefault();
+
+    const title = this.querySelector('.card__title')?.textContent?.trim() ||
+      this.dataset.title ||
+      'Какой-то Ресторан';
+
+    try {
+      localStorage.setItem('restaurantTitle', title);
+      window.location.href = 'restaurant.html';
+    } catch (error) {
+      console.error('Ошибка при переходе:', error);
+    }
+  }
+
+  function initRestaurantPage() {
+    let title = 'Ресторан';
+
+    try {
+      title = localStorage.getItem('restaurantTitle') || title;
+      localStorage.removeItem('restaurantTitle');
+    } catch (error) {
+      console.error('error', error);
+    }
+
+    const titleElement = document.querySelector('.title-section');
+    if (titleElement) {
+      titleElement.textContent = title;
+    }
+    document.title = `${title} - Наш Ресторан`;
+  }
+  initCards()
+  initRestaurantPage()
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
